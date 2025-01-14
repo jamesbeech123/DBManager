73afda379171c9444010bc17a13350efb3cc0019
james <jamesbeech2003@hotmail.com>
2025-01-14 21:15:21 +0000

    -Updated Project to Maven in Intellij

---
+++ src/main/java/dbmanager/DashboardGUI.java
@@ -0,0 +1,20 @@
+package dbmanager;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class DashboardGUI extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
+        primaryStage.setScene(new Scene(loader.load(), 800, 600));
+        primaryStage.setTitle("JavaFX Application");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
